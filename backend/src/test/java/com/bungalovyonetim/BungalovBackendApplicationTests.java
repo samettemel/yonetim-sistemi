dcbd4737ac3aa94da0e59bcf42664191ce8e81e9
Samettml12 <temel.samet@ogr.ahievran.edu.tr>
2025-10-07 20:31:04 +0300

    spring boot kurulumu ve db yapılandırılması gerçekleştirildi

---
+++ backend/src/test/java/com/bungalovyonetim/BungalovBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bungalovyonetim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BungalovBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
